c3216a3b9aa3084dfd07ddf5a9dc21f7312ab1e7
Luís Felipe <luisf.csdev@gmail.com>
2025-12-22 17:49:42 -0300

    feat(ui): add fonts and hero images

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -65,11 +65,11 @@ function PaginationNumber({
   isActive: boolean
 }) {
   const className = clsx(
-    'flex h-10 w-10 items-center justify-center text-sm border',
+    'flex h-10 w-10 items-center justify-center border text-sm',
     {
       'rounded-l-md': position === 'first' || position === 'single',
       'rounded-r-md': position === 'last' || position === 'single',
-      'z-10 bg-blue-600 border-blue-600 text-white': isActive,
+      'z-10 border-blue-600 bg-blue-600 text-white': isActive,
       'hover:bg-gray-100': !isActive && position !== 'middle',
       'text-gray-300': position === 'middle',
     },

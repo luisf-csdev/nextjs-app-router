860b41f629eb84963248866b1379a860f6a529ba
Luís Felipe <luisf.csdev@gmail.com>
2025-12-22 19:05:41 -0300

    feat(ui): fetch dashboard overview data

--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 
 import { LatestInvoice } from '@/app/lib/definitions'
 import { lusitana } from '@/app/ui/fonts'
-export default async function LatestInvoices({
+export async function LatestInvoices({
   latestInvoices,
 }: {
   latestInvoices: LatestInvoice[]
@@ -15,9 +15,7 @@ export default async function LatestInvoices({
         Latest Invoices
       </h2>
       <div className="flex grow flex-col justify-between rounded-xl bg-gray-50 p-4">
-        {/* NOTE: Uncomment this code in Chapter 7 */}
-
-        {/* <div className="bg-white px-6">
+        <div className="bg-white px-6">
           {latestInvoices.map((invoice, i) => {
             return (
               <div
@@ -52,9 +50,9 @@ export default async function LatestInvoices({
                   {invoice.amount}
                 </p>
               </div>
-            );
+            )
           })}
-        </div> */}
+        </div>
         <div className="flex items-center pt-6 pb-2">
           <ArrowPathIcon className="h-5 w-5 text-gray-500" />
           <h3 className="ml-2 text-sm text-gray-500">Updated just now</h3>
